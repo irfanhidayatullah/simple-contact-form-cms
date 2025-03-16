9d561296ef8e642e68777818cfd9c332dd9ffb46
irfanhidayatullah <irfanhidayatullah.jobs@gmail.com>
2025-03-16 14:09:02 +0700

    feat: views

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="bg-[#14213D] text-white py-4 mt-auto">
+    <div class="container mx-auto text-center">
+        <p>
+            &copy; 2025 - 
+            <a href="https://irfanhidayatullah.my.id/" 
+               target="_blank"
+               class="text-white hover:underline">
+               Irfan Hidayatullah
+            </a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="bg-[#14213D] p-4">
+    <div class="container mx-auto flex gap-1.5">
+        <div class="h-8 w-8">
+            <img src="Logo - Simple Contact Form - Irfan Hidayatullah.svg" alt="">
+        </div>
+        <a href="{{ route('contacts.index') }}" class="text-white text-xl font-bold">CMS Simple Contact Form</a>
+    </div>
+</nav>
